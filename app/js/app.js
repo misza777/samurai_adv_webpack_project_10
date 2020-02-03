4658adae4c65446a3469ccb5affda794e67837fc
Michal Nowak <miau.nowak@gmail.com>
2020-02-03 16:57:08 +0100

    object Dog based on arrow functions

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,77 @@
+// 'use strict'
+
 import '../sass/index.scss';
 
-// funkcja strzalkowa
-const listBreeds = () => {
-    const url = 'https://dog.ceo/api/breeds/list/all';
-    fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-        // console.log(data.message);
-        data.message;
-    })
-    .catch(err => console.log(err));
-}
+class Dog {
+    constructor() {
+        // czesc adresu wyciagamy do zmennej
+        this.apiUrl = "https://dog.ceo/api";
+        // console.log(this.apiUrl);
+        // console.log(this);
+        this.imgEl = document.querySelector('.featured-dog img');
+        this.init();
+    }
+    
+    listBreeds = () => {
+        const url = `${this.apiUrl}/breeds/list/all`;
+        // console.log(url);
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data.message);
+            data.message;
+        })
+        .catch(err => console.error('masz kurwa error:', err));
+    }
+
+    getRandomImage = () => {
+        fetch(`${this.apiUrl}/breeds/image/random`)
+            .then(resp => resp.json())
+            .then(data => {data.message;
+                const newImg = document.createElement('img');
+                newImg.setAttribute('src', data.message);
+                document.body.appendChild(newImg);
+            })
+            .catch(err => { 
+                console.log(err);
+                })}
+
+    getRandomImageByBreed = (breed) => {
+        fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(resp => resp.json())
+            .then(data => {
+                data => data.message;
+                console.log(data.message);
+                this.imgEl.setAttribute('src', data.message);
+            })
+    };
+
+init() {
+    this.getRandomImage();
+    this.listBreeds();
+    this.getRandomImageByBreed('bulldog');
+}    
+    }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Dog();
+});
+
+
+
+// wersja poprzednia - funkcje bez klas
+
+// const listBreeds = () => {
+//     const url = 'https://dog.ceo/api/breeds/list/all';
+//     fetch(url)
+//     .then(resp => resp.json())
+//     .then(data => {
+//         // console.log(data.message);
+//         data.message;
+//     })
+//     .catch(err => console.log(err));
+// }
 
 // funkcja tradycyjna 
 // function listBreeds() {
@@ -25,21 +86,21 @@ const listBreeds = () => {
 //funkcje trzeba zawsze wywolac 
 // listBreeds()
 
-const imgTag = document.querySelector('img');
+// const imgTag = document.querySelector('img');
 // console.log(imgTag);
 
-const getRandomImage = () => {
-    const url = 'https://dog.ceo/api/breeds/image/random'
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => {data.message;
-            console.log(data.message);
-            imgTag.setAttribute('src', data.message);
-            // const newImg = document.createElement('img');
-            // newImg.setAttribute('src', data.message);
-            // document.body.appendChild(newImg);
-        })
-}
+// const getRandomImage = () => {
+//     const url = 'https://dog.ceo/api/breeds/image/random'
+//     fetch(url)
+//         .then(resp => resp.json())
+//         .then(data => {data.message;
+//             console.log(data.message);
+//             imgTag.setAttribute('src', data.message);
+//             // const newImg = document.createElement('img');
+//             // newImg.setAttribute('src', data.message);
+//             // document.body.appendChild(newImg);
+//         })
+// }
 
 //funkcje trzeba wywolac
 // getRandomImage()
@@ -56,18 +117,18 @@ const getRandomImage = () => {
 //     .then(imgSrc => imgTag.setAttribute("src", imgSrc))
 
 
-const getRandomImageByBreed = (breed) => {
+// const getRandomImageByBreed = (breed) => {
     
-    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
-            data => data.message;
-            console.log(data.message);
-            imgTag.setAttribute('src', data.message);
-        })
-}
+//     const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+//     fetch(url)
+//         .then(resp => resp.json())
+//         .then(data => {
+//             data => data.message;
+//             console.log(data.message);
+//             imgTag.setAttribute('src', data.message);
+//         })
+// }
 
-getRandomImageByBreed('bulldog/boston')
+// getRandomImageByBreed('bulldog/boston')
 
     
